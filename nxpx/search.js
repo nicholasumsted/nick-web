2459a2751470e8ff1998e535e1542a3aafda8298
Nicholas Umsted <nicholasumsted@gmail.com>
2014-08-26 07:27:15 -0500

    done

---
+++ nxpx/search.js
@@ -0,0 +1,61 @@
+var searcher = {
+    searchInput: null,
+    searchResults: null,
+
+    startSearch: function (terms) {
+        var self = this;
+        this.callSearch(terms).done(
+            function (data) {
+                self.displayResults(data.RelatedTopics);
+            }
+        ).fail(
+            function (p1, message) {
+                self.displayMessage(message);
+            }
+        );
+    },
+
+    callSearch: function (terms) {
+        var duckUrl = 'http://api.duckduckgo.com';
+        return $.ajax({
+            type: 'GET',
+            url: duckUrl,
+            data: {
+                q: terms,
+                format: 'json',
+                pretty: 1
+            },
+            jsonpCallback: 'jsonp',
+            dataType: 'jsonp'
+        });
+    },
+
+    displayResults: function (topics) {
+        $(this.searchResults).html('<br/>');
+        for (var topic in topics) {
+            if (topics[topic].Result !== undefined) {
+                $(this.searchResults).append(topics[topic].Result + '<br/><br/>');
+            }
+        }
+    },
+
+    displayMessage: function (message) {
+        $(this.searchResults).html(message);
+    },
+
+    isEnter: function (ev) {
+        if (ev) {
+            var charCode = ev.keyCode || ev.which;
+            if (charCode == 13) {
+                searcher.startSearch($(this).val());
+                return false;
+            }
+        }
+    },
+
+    bindToSearch: function (searchInputSelector, searchResultsSelector) {
+        this.searchInput = $(searchInputSelector);
+        $(this.searchInput).bind('keypress', searcher.isEnter);
+        this.searchResults = $(searchResultsSelector);
+    }
+}
